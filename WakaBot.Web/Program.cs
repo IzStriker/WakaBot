ab54d366f24b1291b4e433b344d361e910e07cde
IzStriker <zjac104@live.rhul.ac.uk>
2022-12-25 17:43:49 +0000

    fix: project reference and remove comments

--- WakaBot.Web/Program.cs
+++ WakaBot.Web/Program.cs
@@ -2,8 +2,6 @@ using WakaBot.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddHostedService(services => new WakaBot.Core.WakaBot(args));
-//builder.Services.AddHostedService<WakaBot.Core.WakaBot>();
 builder.Services.AddHostedService<WakaBotService>();
 
 var app = builder.Build();
