6219a378f8202061efcb68a1b0f4a65734d84b04
IzStriker <zjac104@live.rhul.ac.uk>
2023-01-21 09:31:48 +0000

    fix: race condition in message queue

--- WakaBot.Core/MessageBroker/MessageQueue.cs
+++ WakaBot.Core/MessageBroker/MessageQueue.cs
@@ -1,37 +1,33 @@
 using System.Collections.Concurrent;
-using System.Reflection;
 
 namespace WakaBot.Core.MessageBroker;
 
 public class MessageQueue
 {
-    private readonly BlockingCollection<object> _queue;
+    private ConcurrentDictionary<string, List<Func<object, Task>>> _channelSubscriptions;
 
     public MessageQueue()
     {
-        _queue = new BlockingCollection<object>();
+        _channelSubscriptions = new ConcurrentDictionary<string, List<Func<object, Task>>>();
     }
 
     public void Subscribe<T>(string channel, Func<T, Task> action) where T : class
     {
-        Task.Run(() =>
+        if (!_channelSubscriptions.ContainsKey(channel))
         {
-            foreach (var message in _queue.GetConsumingEnumerable())
-            {
-                if (message is Message<T> && ((Message<T>)message).Channel == channel)
-                {
-                    action(((Message<T>)message).Payload);
-                }
-            }
-        });
+            _channelSubscriptions[channel] = new List<Func<object, Task>>();
+        }
+        _channelSubscriptions[channel].Add(async (msg) => await action((T)msg));
     }
 
     public void Send<T>(string channel, T message) where T : class
     {
-        _queue.Add(new Message<T>
+        if (_channelSubscriptions.ContainsKey(channel))
         {
-            Channel = channel,
-            Payload = message
-        });
+            foreach (var action in _channelSubscriptions[channel])
+            {
+                Task.Run(() => action(message));
+            }
+        }
     }
 }
